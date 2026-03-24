707084fba11fc12dcf706351a16d127966df588f
Luis Gabriel Goés <luisgabriel533@gmail.com>
2026-03-24 15:38:50 -0300

    feat: Project start

---
+++ src/main/java/br/goes/luis/application/Application.java
@@ -0,0 +1,28 @@
+package br.goes.luis.application;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@SpringBootApplication
+@EnableAsync
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+public class Application {
+
+    static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
